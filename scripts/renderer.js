a8e8e922f829fb5112e794b4726fd2c515d2d8b2
Doncho Minkov <DonchoMinkov@gmail.com>
2019-06-04 22:22:22 +0300

    Moving dot

---
+++ scripts/renderer.js
@@ -0,0 +1,29 @@
+(function () {
+    const init = function (ctx, bounds) {
+        this.ctx = ctx;
+        this.bounds = bounds;
+        return this;
+    };
+
+    const clear = function () {
+        const { ctx } = this;
+        const { width, height } = this.bounds;
+        ctx.clearRect(0, 0, width, height);
+    };
+
+    const rendererDot = function (left, top) {
+        const { ctx } = this;
+        ctx.fillRect(left, top, 15, 15);
+    };
+
+    const renderer = {
+        init,
+        rendererDot,
+        clear,
+        // renderPlayer
+        // renderBullets
+        // renderEnemies
+    };
+
+    window.renderer = renderer;
+}());
